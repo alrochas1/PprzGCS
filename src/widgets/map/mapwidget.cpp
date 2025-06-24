126d0782b80a6ced82dac2987f91e36610c8c692
Alejandro Rochas <alrochas@ucm.es>
2025-06-24 12:13:17 +0200

    Optimized to only render the cell that changes

--- src/widgets/map/mapwidget.cpp
+++ src/widgets/map/mapwidget.cpp
@@ -1568,7 +1568,8 @@ void MapWidget::onGridChanges(QString sender, pprzlink::Message msg) {
     msg.getField("value", value);
 
     obstacle_grid_map->updateCell(row, column, value);
-    grid_item->updateGraphics(this, UpdateEvent::ITEM_CHANGED);
+    // grid_item->updateGraphics(this, UpdateEvent::ITEM_CHANGED);
+    grid_item->updateCell(this, UpdateEvent::ITEM_CHANGED, row, column);
 
 
 }
